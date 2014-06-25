5a774d99ad62889db400a8b80fabae944bbbbab3
T107 <T107@PC261>
2014-06-24 21:43:39 -0500

    Jason strikes

--- src/main/java/slr/epoo/web/cnt/PersonaCntr.java
+++ src/main/java/slr/epoo/web/cnt/PersonaCntr.java
@@ -6,6 +6,11 @@
 
 package slr.epoo.web.cnt;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import org.codehaus.jackson.JsonEncoding;
+import org.codehaus.jackson.JsonGenerator;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,6 +26,18 @@ public class PersonaCntr {
     @RequestMapping(method=RequestMethod.GET, value="/todos", headers = {"Accept=text/html"})
     public @ResponseBody String resp(){
         return "probando metodo todos";
+    }
+    
+    @RequestMapping(method = RequestMethod.GET, value="/usuario", headers = {"Accept=Application/JSON"})
+    public @ResponseBody String userJSON() throws IOException{
+        String res;
+        ObjectMapper mp = new ObjectMapper();
+        try(ByteArrayOutputStream out = new ByteArrayOutputStream()){
+            JsonGenerator jg = mp.getJsonFactory().createJsonGenerator(out, JsonEncoding.UTF8);
+            jg.writeObject(new slr.epoo.web.mdl.Usuario());
+            res = out.toString();
+        }
+        return res;
     }
     
 }
---
+++ src/main/java/slr/epoo/web/mdl/Usuario.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package slr.epoo.web.mdl;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author Aaron
+ */
+@Entity
+@Table(name = "usuario")
+@NamedQueries({
+    @NamedQuery(name = "Usuario.findAll", query = "SELECT u FROM Usuario u"),
+    @NamedQuery(name = "Usuario.findByNombre", query = "SELECT u FROM Usuario u WHERE u.nombre = :nombre"),
+    @NamedQuery(name = "Usuario.findByPaterno", query = "SELECT u FROM Usuario u WHERE u.paterno = :paterno"),
+    @NamedQuery(name = "Usuario.findByUsuario", query = "SELECT u FROM Usuario u WHERE u.usuario = :usuario"),
+    @NamedQuery(name = "Usuario.findByPassword", query = "SELECT u FROM Usuario u WHERE u.password = :password"),
+    @NamedQuery(name = "Usuario.findByRolAdmin", query = "SELECT u FROM Usuario u WHERE u.rolAdmin = :rolAdmin")})
+public class Usuario implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 80)
+    @Column(name = "nombre")
+    private String nombre;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 80)
+    @Column(name = "paterno")
+    private String paterno;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 10)
+    @Column(name = "usuario")
+    private String usuario;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 10)
+    @Column(name = "password")
+    private String password;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "rol_admin")
+    private char rolAdmin;
+
+    public Usuario() {
+    }
+
+    public Usuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public Usuario(String usuario, String nombre, String paterno, String password, char rolAdmin) {
+        this.usuario = usuario;
+        this.nombre = nombre;
+        this.paterno = paterno;
+        this.password = password;
+        this.rolAdmin = rolAdmin;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPaterno() {
+        return paterno;
+    }
+
+    public void setPaterno(String paterno) {
+        this.paterno = paterno;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public char getRolAdmin() {
+        return rolAdmin;
+    }
+
+    public void setRolAdmin(char rolAdmin) {
+        this.rolAdmin = rolAdmin;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (usuario != null ? usuario.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Usuario)) {
+            return false;
+        }
+        Usuario other = (Usuario) object;
+        if ((this.usuario == null && other.usuario != null) || (this.usuario != null && !this.usuario.equals(other.usuario))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "slr.epoo.web.mdl.Usuario[ usuario=" + usuario + " ]";
+    }
+    
+}
